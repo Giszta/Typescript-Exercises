ffcef50985d79895f4b4a3140486354912e11751
AdamGiszter <a.m.giszter@gmail.com>
2024-02-18 19:52:54 +0100

    Array

--- task 01/dist/app.js
+++ task 01/dist/app.js
@@ -1,36 +1,25 @@
-// let age: number = 29; //w przypadku kiedy podajemy wartosc zmiiennej, nie musimy okreslac jej typu, ponieważ ts zrobi to sam (type inference)
-// let ageAsString: string = "dziesiec";
-// const add = (v1: number, v2: number) => v1 + v2;
-// console.log(add(10, 5));
-// const input1Element: HTMLInputElement = document.querySelector("#input1");
-// const input2Element: HTMLInputElement = document.querySelector("#input2");
-// const addButtonElement = document.querySelector("button");
-// const addInput = (v1: number, v2: number) => v1 + v2;
-// addButtonElement.addEventListener("click", () => {
-// 	const sum = addInput(
-// 		Number(input1Element.value),
-// 		Number(input2Element.value)
-// 	);
-// 	console.log(sum);
-// });
-// //union type - związek kilku typów
-// const logAge = (age2: number | string) => {
-// 	console.log(`Hej, mam ${age2} lat`);
-// };
-// logAge(10);
-// logAge("dwadziescia osiem");
-// let test: string | number | boolean;
-// test = "test";
-// test = 21;
-// test = true;
-// // test = [];
-// task 1 - discount
-const buttonElement = document.querySelector("button");
-const calculatePrice = (originalPrice, hasDiscount) => {
-    return hasDiscount ? originalPrice * 0.8 : originalPrice;
+// <li>
+// <label for="task-1">Wyrzucić śmieci</label>
+// <input type="checkbox" id="task-1" name="Wyrzucić śmieci" />
+// </li>
+const taskNameInputElement = document.querySelector("#name");
+const addButtonElement = document.querySelector("button");
+const tasksContainerElement = document.querySelector(".tasks");
+const tasks = ["Wyrzucić śmieci", "Pójść na siłkę", "nakarmić koty"];
+const render = () => {
+    tasksContainerElement.innerHTML = "";
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.innerHTML = task;
+        tasksContainerElement.appendChild(taskElement);
+    });
 };
-buttonElement.addEventListener("click", () => {
-    const originalPrice = 50;
-    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
-    console.log(calculatePrice(originalPrice, hasDiscount));
+const addTask = (task) => {
+    tasks.push(task);
+};
+addButtonElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    addTask(taskNameInputElement.value);
+    render();
 });
+render();
