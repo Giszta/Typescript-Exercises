2e8d9106a185d1dd21af5e337aa5fdaeee27fe17
AdamGiszter <a.m.giszter@gmail.com>
2024-02-28 09:36:30 +0100

    function

--- task 01/src/app.ts
+++ task 01/src/app.ts
@@ -1,17 +1,13 @@
+import { renderCategories } from "./helpers/render-categories.js";
+import { render } from "./helpers/render-task.js";
+import { Category, Task } from "./types/types";
+
 const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
 const addButtonElement: HTMLElement = document.querySelector("button");
 const tasksContainerElement: HTMLElement = document.querySelector(".tasks");
 const categoriesContainerElement: HTMLElement =
 	document.querySelector(".categories");
 let selectedCategory: Category;
-
-type Category = "general" | "work" | "gym" | "hobby";
-
-interface Task {
-	title: string;
-	done: boolean;
-	category?: Category;
-}
 
 const categories: Category[] = ["general", "work", "gym", "hobby"];
 
@@ -21,58 +17,12 @@ const tasks: Task[] = [
 	{ title: "nakarmić koty", done: false },
 ];
 
-const render = () => {
-	tasksContainerElement.innerHTML = "";
-	tasks.forEach((task, index) => {
-		const taskElement: HTMLElement = document.createElement("li");
-		if (task.category) {
-			taskElement.classList.add(task.category);
-		}
-		const id: string = `task-${index}`;
-		const labelElement: HTMLLabelElement = document.createElement("label");
-		labelElement.innerText = task.title;
-		labelElement.setAttribute("for", id);
-		const checkboxElement: HTMLInputElement = document.createElement("input");
-		checkboxElement.type = "checkbox";
-		checkboxElement.name = task.title;
-		checkboxElement.id = id;
-		checkboxElement.checked = task.done;
-		checkboxElement.addEventListener("change", () => {
-			task.done = !task.done;
-		});
-
-		taskElement.appendChild(labelElement);
-		taskElement.appendChild(checkboxElement);
-
-		tasksContainerElement.appendChild(taskElement);
-	});
-};
-
-const renderCategories = () => {
-	categories.forEach((category) => {
-		const categoryElement: HTMLElement = document.createElement("li");
-
-		const radioInputElement: HTMLInputElement = document.createElement("input");
-		radioInputElement.type = "radio";
-		radioInputElement.name = "category";
-		radioInputElement.value = category;
-		radioInputElement.id = `category-${category}`;
-		radioInputElement.addEventListener("change", () => {
-			selectedCategory = category;
-		});
-
-		const labelElement: HTMLLabelElement = document.createElement("label");
-		labelElement.setAttribute("for", `category-${category}`);
-		labelElement.innerText = category;
-
-		categoryElement.appendChild(radioInputElement);
-		categoryElement.appendChild(labelElement);
-
-		categoriesContainerElement.appendChild(categoryElement);
-	});
-};
 const addTask = (task: Task) => {
 	tasks.push(task);
+};
+
+const updateSelectedCategory = (newCategory: Category) => {
+	selectedCategory = newCategory;
 };
 
 addButtonElement.addEventListener("click", (event: Event) => {
@@ -82,8 +32,12 @@ addButtonElement.addEventListener("click", (event: Event) => {
 		done: false,
 		category: selectedCategory,
 	});
-	render();
+	render(tasks, tasksContainerElement);
 });
 
-renderCategories();
-render();
+renderCategories(
+	categories,
+	categoriesContainerElement,
+	updateSelectedCategory
+);
+render(tasks, tasksContainerElement);
