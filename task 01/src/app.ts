be74e0655acadf8976341785aecf96010d8aaa7d
AdamGiszter <a.m.giszter@gmail.com>
2024-03-13 01:57:36 +0100

    tuples / interface vs type alias

--- task 01/src/app.ts
+++ task 01/src/app.ts
@@ -41,6 +41,16 @@ addButtonElement.addEventListener("click", (event: Event) => {
 	render(tasks, tasksContainerElement);
 });
 
+type TaskAsTuple = [string, Category, boolean];
+
+const task: TaskAsTuple = ["zrobić klate", Category.GYM, false];
+
+const taskName = task[0];
+const taskCategory = task[1];
+const taskDoneStatus = task[2];
+
+addTask({ title: taskName, category: taskCategory, done: taskDoneStatus });
+
 renderCategories(
 	categories,
 	categoriesContainerElement,
@@ -60,3 +70,30 @@ render(tasks, tasksContainerElement);
 // if (typeof value === "number") {
 // 	logFixed(value);
 // }
+
+// Interface vs type alias
+// type TaskType = {
+// 	name: string;
+// 	done: boolean;
+// 	category?: Category;
+// };
+
+// interface TaskInterface {
+// 	name: string;
+// 	done: boolean;
+// 	category?: Category;
+// }
+// interface TaskInterface {
+// 	createdAt: Date;
+// }
+
+// let newTask: TaskInterface;
+
+// newTask = {
+// 	name: "nowy task",
+// 	done: true,
+// 	createdAt: new Date(),
+// };
+
+// type Category2 = "work" | "hobby" | "general";
+// type Category2AndNumber = Category | number;
