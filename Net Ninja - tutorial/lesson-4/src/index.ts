f5a552cab287beeb0ffe01f9e830e3448cb88b84
AdamGiszter <116649056+Giszta@users.noreply.github.com>
2024-03-15 13:21:48 +0100

    arrays, objects

---
+++ Net Ninja - tutorial/lesson-4/src/index.ts
@@ -0,0 +1,40 @@
+//arrays
+
+let names: string[] = ["Mario", "Luigi", "Peach"];
+let ages: number[] = [25, 29, 15];
+
+names.push("Adam");
+ages.push(45);
+console.log(names);
+console.log(ages);
+
+// type inference with arrays
+
+let fruits = ["apples", "pears", "bananas", "mangos"];
+fruits.push("peaches");
+
+const f = fruits[3];
+let things = [1, true, "hello"];
+
+const t = things[0];
+
+//object literals
+
+let user: {
+	firstName: string;
+	age: number;
+	id: number;
+} = { firstName: "Mario", age: 30, id: 1 };
+
+user.firstName = "peach";
+// user.email = "asda@gmail.com";
+user.id = 2;
+
+// type interface with object literals
+let person = {
+	name: "luigi",
+	score: 35,
+};
+
+// person.name = true
+person.name = "Adam";
